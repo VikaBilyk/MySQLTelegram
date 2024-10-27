6ccbc293988b8421e775a01af562413e70ebcbc9
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2024-10-27 17:17:29 +0200

    Add files via upload

--- clear.sql
+++ clear.sql
@@ -1,11 +1,11 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
-delete from Users where IsDeleted = true;
-delete from Chats where IsDeleted = true;
-delete from Messages where IsDeleted = true;
-delete from Contacts where IsDeleted = true;
-delete from Files where IsDeleted = true;
-delete from Reactions where IsDeleted = true;
-delete from `groups` where IsDeleted = true;
+TRUNCATE table Users;
+TRUNCATE table Chats;
+TRUNCATE table Messages;
+TRUNCATE table Contacts;
+TRUNCATE table Files;
+TRUNCATE table Reactions;
+TRUNCATE table `groups`;
 
 SET FOREIGN_KEY_CHECKS = 1;
